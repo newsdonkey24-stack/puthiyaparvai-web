94def3fa0aab9297a7a3994ce5d5e3cfc29880e7
newsdonkey24-stack <newsdonkey24@gmail.com>
2026-02-10 10:35:54 +0530

    Add script.js

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
 fetch("news.json")
   .then(res => res.json())
   .then(data => {
-
     const container = document.querySelector(".container");
     if (!container) return;
 
-    container.innerHTML = ""; // clear old content
+    container.innerHTML = "";
 
-    // First: Breaking news
-    const breaking = data.filter(n => n.breaking === true);
+    const breakingNews = data.filter(n => n.breaking === true);
+    const normalNews = data.filter(n => !n.breaking);
 
-    // Then: Normal news
-    const normal = data.filter(n => !n.breaking);
-
-    [...breaking, ...normal].forEach(news => {
-
+    [...breakingNews, ...normalNews].forEach(news => {
       const div = document.createElement("div");
       div.className = "news-item";
 
       div.innerHTML = `
         ${news.breaking ? <span class="breaking">முக்கிய செய்தி</span> : ""}
-
         <img src="${news.image}" class="thumb" alt="${news.title}">
-
         <h2>
           <a href="${news.link}">
             ${news.title}
           </a>
         </h2>
-
         <p>${news.summary}</p>
       `;
 
       container.appendChild(div);
-
     });
-
   })
-  .catch(err => {
-    console.error("News load error:", err);
-  });
+  .catch(err => console.error("News load error:", err));
