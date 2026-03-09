979d7e4fe03487b99ba86a4cb320123e91cee427
newsdonkey24-stack <newsdonkey24@gmail.com>
2026-03-09 18:57:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -132,16 +132,15 @@ filteredData.forEach(news => {
 
 html += `
 <div style="display:flex;gap:15px;margin-bottom:20px;border-bottom:1px solid #ddd;padding-bottom:10px;align-items:center;">
-<a href="news.html?id=${news.id}" style="text-decoration:none;color:#000;display:flex;gap:15px;align-items:center;">
+<a href="news/news.html?id=${news.id}" style="text-decoration:none;color:#000;display:flex;gap:15px;align-items:center;">
 
-<img src="../${news.image}" style="width:120px;height:80px;object-fit:cover;border-radius:6px;">
+<img src="${news.image}" style="width:120px;height:80px;object-fit:cover;border-radius:6px;">
 
 <h4 style="margin:0;font-size:18px;line-height:1.4;">${news.title}</h4>
 
 </a>
 </div>
 `;
-
 container.innerHTML = html;
 
 }
